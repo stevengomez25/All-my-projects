a6688527f77d45463bbd6a34c18c4e086c13a51a
Steven Gomez <brastegocha@gmail.com>
2024-04-02 21:51:17 -0500

    updates in follow mouse

--- projects_react/03-mouse-follower/src/App.jsx
+++ projects_react/03-mouse-follower/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+
+  const [position, setPosition] = useState({x:0,y:0})
+  
+  useEffect(() => {
+    console.log('Effect', {enabled},[enabled])
+
+    const handleMove = (event) =>{
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x:clientX, y:clientY})
+    }
+    if (enabled) {
+      window.addEventListener('pointermove',handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove',handleMove)
+      
+    }
+  },[enabled])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor : '#09f',
+        borderRadius : '50%',
+        opacity : 0.8,
+        pointerEvents: 'none',
+        left : -20,
+        top : -20,
+        width : 40,
+        height : 40,
+        transform : `translate(${position.x}px, ${position.y}px)`
+        }
+      }></div>
+      <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} Seguir Puntero
+          </button>
+    </main>
   )
 }
-
 export default App
