121067b91392ea82fb63c0a7a7102d03f2748323
Steven Gomez <brastegocha@gmail.com>
2024-05-17 23:13:35 -0500

    Styles Hoobies

--- portfolio 1.1/code.js
+++ portfolio 1.1/code.js
@@ -274,17 +274,19 @@ About_me.addEventListener("click", () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fringilla eros et ex tristique, quis laoreet magna aliquet. Nullam pretium urna vitae ligula faucibus hendrerit. Etiam egestas enim nec consequat rutrum. Fusce gravida sodales purus vitae vulputate. In imperdiet, erat ac porttitor rutrum, ex quam consectetur magna, quis consequat metus ligula id libero. Nunc ac luctus lorem. Donec sed lacinia nisl, id ornare nisl. Nunc semper neque ut dolor mattis, eget blandit ex facilisis. Nunc cursus nisl quam, vel lacinia erat rutrum quis. Vivamus et risus lectus. Praesent at ex tincidunt arcu feugiat cursus a nec sapien. Nullam tempus est ut lectus lacinia mattis. Maecenas consectetur ligula mi, non lacinia lorem molestie ac. Suspendisse ullamcorper, mauris in consectetur porttitor, diam leo ornare turpis, at viverra libero libero a eros. Aliquam efficitur elit vel nulla viverra convallis. Pellentesque ut molestie est, a dictum lacus.</p>
             </div>
             `,`
-            <div class="desplegated_container">
-                <h2>titulo</h2>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
-            </div>
-            <div class="desplegated_container">
-                <h2>titulo</h2>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
-            </div>
-            <div class="desplegated_container">
-                <h2>titulo</h2>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
+            <div class="Hobbies">
+                <div class="desplegated_container">
+                    <h2>titulo</h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
+                </div>
+                <div class="desplegated_container">
+                    <h2>titulo</h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
+                </div>
+                <div class="desplegated_container">
+                    <h2>titulo</h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/S%C3%B8rv%C3%A1gsvatn_4.jpg">
+                </div>
             </div>
             `,
             `
