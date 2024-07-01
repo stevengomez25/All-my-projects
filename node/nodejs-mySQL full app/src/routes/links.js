c6403f0684f78555d0d4e9089a4e46a7bf78a3e2
Steven Gomez <brastegocha@gmail.com>
2024-06-30 21:50:53 -0500

    arreglando por fin el problema de comunicacion entre la db

--- node/nodejs-mySQL full app/src/routes/links.js
+++ node/nodejs-mySQL full app/src/routes/links.js
@@ -7,14 +7,15 @@ router.get('/add',(req, res)=>{
     res.render('../views/links/add.hbs');
 })
 
-router.post('/add', async (req, res)=>{
+router.post('/add', async(req, res)=>{
     const {title,url,description} = req.body;
     const NewLink = {
         title,
         url,
         description
     };
-    await pool.query('INSERT INTO links set ?', [NewLink]);
+    await pool.query('INSERT INTO link set ?', [NewLink]);
+    res.send('received');
 })
 
 module.exports = router;
