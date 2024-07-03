58afa96546f9012b9d29a989cca76b81e7407018
Steven Gomez <brastegocha@gmail.com>
2024-07-02 22:11:34 -0500

    agregando proceso signup y validation

--- node/nodejs-mySQL full app/src/lib/passport.js
+++ node/nodejs-mySQL full app/src/lib/passport.js
@@ -4,7 +4,25 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../database');
 const helpers = require('../lib/helpers');
 
-
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+},async(req,username,password,done)=>{
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?',[username]);
+    console.log(req.body);
+    if (rows.length > 0) {
+        const user = rows[0];
+        const valid_password = await helpers.matchPassword(password, user.password);
+        if (valid_password){
+            done(null, user, req.flash('success','Welcome! '+ user.full_name));
+        }else{
+            done(null,false,req.flash('message','Invalid password'));
+        }
+    }else{
+        return done(null,false,req.flash('message','username does not exist'));
+    }
+}));
 
 passport.use('local.signup',new LocalStrategy({
     usernameField: 'username',
