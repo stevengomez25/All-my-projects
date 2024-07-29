05cea81db59b0ff3ccd67e29d98896b8a762a7c2
Steven Gomez <brastegocha@gmail.com>
2024-07-28 21:13:54 -0500

    solo yo me puedo sacar de aqui mismo

--- projects_react/projects/blogger/my-studies-blog/src/App.js
+++ projects_react/projects/blogger/my-studies-blog/src/App.js
@@ -5,6 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+      <nav class="navbar navbar-expand-lg bg-body-tertiary w-100 p-2">
+        <div class="container-fluid">
+          <a class="navbar-brand flex-grow-1 fs-2" href="#">Centro de Estudio</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Notas guardadas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">crear nueva nota</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Notas favoritas</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
         <h1>Aqui tendrás todo tu conocimiento adquirido</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
